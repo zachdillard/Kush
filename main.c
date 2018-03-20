e533e890b414620d243fe5b591b89408c782d235
Pablo Novoa <pnovoa13@hotmail.com>
2018-03-20 16:07:09 -0400

    Added comments for User, Group, Others

--- main.c
+++ main.c
@@ -97,17 +97,17 @@ int l(void){        //to go with ls function as ls -l
                 default:       printf("- "); break;
             }
 
-            printf( (fileStat.st_mode & S_IRUSR) ? "r " : "- ");
+            printf( (fileStat.st_mode & S_IRUSR) ? "r " : "- "); //User permissions
             printf( (fileStat.st_mode & S_IWUSR) ? "w " : "- ");
             printf( (fileStat.st_mode & S_IXUSR) ? "x " : "- ");
-            printf( (fileStat.st_mode & S_IRGRP) ? "r " : "- ");
+            printf( (fileStat.st_mode & S_IRGRP) ? "r " : "- ");// Group permissions
             printf( (fileStat.st_mode & S_IWGRP) ? "w " : "- ");
             printf( (fileStat.st_mode & S_IXGRP) ? "x " : "- ");
-            printf( (fileStat.st_mode & S_IROTH) ? "r " : "- ");
+            printf( (fileStat.st_mode & S_IROTH) ? "r " : "- ");// Others permissions
             printf( (fileStat.st_mode & S_IWOTH) ? "w " : "- ");
             printf( (fileStat.st_mode & S_IXOTH) ? "x " : "- ");
-            printf("\t%d ", fileStat.st_nlink);
-            printf("%lld",fileStat.st_size);
+            printf("\t%d ", fileStat.st_nlink); //link number
+            printf("%lld",fileStat.st_size); //size
             printf("\n");
 
 
