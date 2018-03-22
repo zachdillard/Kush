ef906df307a771670a0e6e9207d0083106f5c857
Zach Dillard <zachdillard@users.noreply.github.com>
2018-03-22 16:15:24 -0400

    Create mycommands.h

---
+++ mycommands.h
@@ -0,0 +1,29 @@
+/***************************************************
+    mycommands.h
+    Header file for the Kennesaw Shell commands
+ ***************************************************/
+
+//Displays contents of file
+//Author: Brett Warner
+int cat(const char* filename);
+
+//Displays contents of current directory
+//Author: Pablo Novoa
+int ls(void);
+
+//Helper function to ls
+//Author: Pablo Novoa
+int l(void);
+
+//Copy contents of one file to another location
+//Author: Jacob Crumbley
+int cp(const char* src, const char* dest);
+
+//Searches for a string in a file and if found,
+//returns the line its found on
+//Author: Zach Dillard
+int grep(const char* input);
+
+//Display available commands
+//Author: Zach Dillard
+void help(void);
