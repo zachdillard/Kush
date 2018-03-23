bf1cef3c7c7b794aaa19bdcec85c67c94270dd3b
Zach Dillard <zachdillard@users.noreply.github.com>
2018-03-22 20:21:37 -0400

    Update myshell.cpp

--- myshell.cpp
+++ myshell.cpp
@@ -50,6 +50,13 @@ int main() {
                 printf("cp: %s: not a valid file or directory\n", src);
         }
         else if (strcmp(token, "grep") == 0) {
+            int argc = 0;
+            char* cmp = input;
+            while(strtok(cmp, d) != NULL) {
+                argc++;
+                strtok(NULL, d);
+            }
+            printf("%d", argc);
             char* search = strtok(NULL, d);
             char* file;
             while((file = strtok(NULL, d)) != NULL) {
